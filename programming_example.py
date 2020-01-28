ea109c40bce0220ceb0a6fb3b50ad4789c70c1fb
Ethan Frye <59927383+fryeem@users.noreply.github.com>
2020-01-28 12:33:27 -0500

    Add files via upload

---
+++ programming_example.py
@@ -0,0 +1,9 @@
+ethan_gpa=3.0   
+andrew_gpa=3.0
+if andrew_gpa > ethan_gpa:
+    andrew_gpa=andrew_gpa+0.05
+else:
+    ethan_gpa=ethan_gpa+0.01
+print(ethan_gpa)
+print(andrew_gpa)
+print(andrew_gpa-ethan_gpa)
