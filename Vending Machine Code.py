a8576b22e710c55987248ad7e25489dc68765495
Ethan Frye <59927383+fryeem@users.noreply.github.com>
2020-11-01 20:00:17 -0500

    Update Vending Machine Code.py

--- Vending Machine Code.py
+++ Vending Machine Code.py
@@ -1,84 +1,76 @@
 ItemList=['1. Snickers','2. Kit Kat','3. Reecees','4. Milky Way','5. Twix']
-print(ItemList[0])
-print(ItemList[1])
-print(ItemList[2])
-print(ItemList[3])
-print(ItemList[4])
 
 Price = {
-    'Snickers':1.25,
-    'Kit Kat': 1.25,
-    'Reesees':1.25,
-    'Milky Way':1.25,
-    'Twix':1.25,
+    '1. Snickers':1.25,
+    '2. Kit Kat': 1.25,
+    '3. Reesees':1.25,
+    '4. Milky Way':1.25,
+    '5. Twix':1.25,
 }
 
 QuantityAvailable = {
-        'Snickers':10,
-        'Kit Kat':10,
-        'Reecees':10,
-        'Milky Way':10,
-        'Twix':10,
+        '1. Snickers':10,
+        '2. Kit Kat':10,
+        '3. Reecees':10,
+        '4. Milky Way':10,
+        '5. Twix':10,
 }
 
-Item = input("Enter choice(1/2/3/4/5): ")
+print("Hello!")
+print("Welcome to Virtual Vending")
 
-while True:
-    if Item in ('1'):
-        print('Snickers')
-        print(Price['Snickers'])
-        print(QuantityAvailable['Snickers'])
-    elif Item in ('2'):
-        print("Kit Kat")
-        print(Price['Kit Kat'])
-        print(QuantityAvailable['Kit Kat'])
-    elif Item in ('3'):
-        print("Reecees")
-        print(Price['Reecees'])
-        print(QuantityAvailable['Reecees'])
-    elif Item in ('4'):
-        print("Milky Way")
-        print(Price['Milky Way'])
-        print(QuantityAvailable['Milky Way'])
-    elif Item in ('5'):
-        print("Twix")
-        print(Price['Twix'])
-        print(QuantityAvailable['Twix'])
-    else:
-        print("INVALID SELECTION")
-    break
+answer1=input("Would you like to make a purchase? (Y/N):")
 
-print("COST OF QUANTITY SELECTED")
-x = int(input("Quantity Desired:"))
+def yes_no(answer1):
+        while True:
+                if answer1 == "Y":
+                        print("What would you like to purchase?:")
+                        print(ItemList[0])
+                        print(ItemList[1])
+                        print(ItemList[2])
+                        print(ItemList[3])
+                        print(ItemList[4])
+                        answer2=input("Enter Choice(1/2/3/4/5):") 
+                elif answer1 == "N":
+                        print("Invalid Selection/Goodbye")
+                        answer2='0'
+                else:
+                        print("Invalid Selection/Goodbye")
+                        answer2='0'
+                break
+        return answer2
+answer2 = yes_no(answer1)
 
-while True:
-    if Item in ('1') and x<=10:
-        print(Price['Snickers']*x)
-    elif Item in ('2') and x<=10:
-        print(Price['Kit Kat']*x)
-    elif Item in ('3') and x<=10:
-        print(Price['Reecees']*x)
-    elif Item in ('4') and x<=10:
-        print(Price['Milky Way']*x)
-    elif Item in ('5') and x<=10:
-        print(Price['Twix']*x)
-    else:
-        print("NOT ENOUGH PRODUCT")
-    break
+def selection(answer2):
+    while True:
+        if answer2 in ('1'):
+            print("Snickers")
+            print(Price['1. Snickers'])
+            print(QuantityAvailable['1. Snickers'])
+        elif answer2 in ('2'):
+            print("Kit Kat")
+            print(Price['2. Kit Kat'])
+            print(QuantityAvailable['2. Kit Kat'])
+        elif answer2 in ('3'):
+            print("Reecees")
+            print(Price['3. Reecees'])
+            print(QuantityAvailable['3. Reecees'])
+        elif answer2 in ('4'):
+            print("Milky Way")
+            print(Price['4. Milky Way'])
+            print(QuantityAvailable['4. Milky Way'])
+        elif answer2 in ('5'):
+            print("Twix")
+            print(Price['5. Twix'])
+            print(QuantityAvailable['5. Twix'])
+        elif answer2 in ('0'):
+            print("Invalid Selection/Goodbye")
+        else:
+            print("Invalid Selection/Goodbye")
+        break
+selection(answer2)
 
 
-print("Quantity Remaining")
-while True:
-    if Item in ('1') and x<=10:
-        print(QuantityAvailable['Snickers']-x)
-    elif Item in ('2') and x<=10:
-        print(QuantityAvailable['Kit Kat']-x)
-    elif Item in ('3') and x<=10:
-        print(QuantityAvailable['Reecees']-x)
-    elif Item in ('4') and x<=10:
-        print(QuantityAvailable['Milky Way']-x)
-    elif Item in ('5') and x<=10:
-        print(QuantityAvailable['Twix']-x)
-    else:
-        print("NOT ENOUGH PRODUCT")
-    break
+
+
+                
